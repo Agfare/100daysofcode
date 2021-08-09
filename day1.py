f73e3e75b81e1928cbae2e1ef9e8f0541683c6a5
Alexander Markevitch <alexander.markevitch@gmail.com>
2021-08-09 21:32:20 +0300

    First day commit, no coding exercises

---
+++ day1.py
@@ -0,0 +1,11 @@
+# a simple print
+print("Hello World")
+# a simple print with a new line
+print("Hello World\nHello World")
+# a print with an input and concatenation
+print("Hello" + " " + input("What is your name? "))
+# an input asking about the name
+name = input("What is your name? ")
+print(name)
+# checking the length of an input
+print(len(input("What is your name? ")))
